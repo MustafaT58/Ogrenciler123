d3dc937ad30e8261954591dc1850be46076ee359
SEM <SEM@DESKTOP-4GKM6HK>
2022-10-04 08:46:43 +0300

    Add project files.

---
+++ Ogrenciler123/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FakeData;
+SahteVeri sahteVeri = new SahteVeri();
+Basla();
+void Basla()
+{
+    for (int i = 0; i < 10; i++)
+    {
+        Console.WriteLine($"{i+1} {sahteVeri.AdVeri()} {sahteVeri.SoyadVeri()} {sahteVeri.SehirVeri()}");
+    }
+}
+Console.ReadKey();
